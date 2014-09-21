5b5eeb7ec837879919b04b376b6b6862033799cc
Slava Zubrin <zubrin@qarea.com>
2014-09-21 08:06:39 +0300

    WIP

--- Coursera/algorithms_princeton/part1/week1/Percolation.java
+++ Coursera/algorithms_princeton/part1/week1/Percolation.java
@@ -1,6 +1,12 @@
 public class Percolation {
     
-    private final boolean[][] grid;   // grid with sites either open or blocked
+    private final int topIndex;
+    private final int bottomIndex;
+    
+    private final int problemSize;
+    private final WeightedQuickUnionUF unionUF;
+    private final boolean[][] grid2D;   // grid with sites either open or blocked
+    private final boolean[] grid1D;   // grid with sites either open or blocked
     
     /*
      public Percolation(int N);                // create N-by-N grid, with all sites blocked
@@ -16,44 +22,80 @@ public class Percolation {
   
         if (N <= 0) throw new IllegalArgumentException();
   
-        grid = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                grid[i][j] = false;
+        problemSize = N;
+        topIndex    = problemSize * problemSize + 1;
+        bottomIndex = problemSize * problemSize;
+        
+        /*
+        grid2D = new boolean[N][N];
+        for (int i = 0; i < problemSize; i++) {
+            for (int j = 0; j < problemSize; j++) {
+                grid2D[i][j] = false;
             }
         }
+        */
+        
+        int 1DSize = problemSize * problemSize + 2;
+        
+        grid1D = new boolean[1DSize];
+        for (int i = 0; i < 1DSize; i++) {
+            grid1D[i] = false;
+        }
+        grid1D[bottomIndex] = true;
+        grid1D[topIndex]    = true;
+        
+        // prepare union
+        unionUF = new WeightedQuickUnionUF(1DSize);
+        // connect top and bottom items to 
+        for int (i = 1; i <= problemSize; i++) {
+            unionUF.union(bottomIndex, 1DZeroBasedIndexFrom2DOneBasedIndexes(1, i)); 
+            unionUF.union(topIndex, 1DZeroBasedIndexFrom2DOneBasedIndexes(problemSize, i));
+        }       
     }
     
     // Public methods
     
     public void open(int i, int j) {
-        grid[i][j] = true;
+        grid2D[i][j] = true;
     }
     
     public boolean isOpen(int i, int j) {
-        return grid[i][j];
+        return grid2D[i][j];
     }
     
     public boolean isFull(int i, int j) {
-        return true;
+        boolean result = false;
+        
+//        for (int k = 0; k < N; k++) {
+//            if 
+//        }
+        
+        return result;
     }
     
     public boolean percolates() {
-        return true;
-    }
-    
-    public static void main(String[] args) {
-        
-        Percolation percolationItem = new Percolation(10);
-        // System.out.println(percolationItem.isOpen(3, 3));
-        StdOut.println(percolationItem.isOpen(3, 3));
-        
-        percolationItem.open(3, 3);
-        // System.out.println(percolationItem.isOpen(3, 3));
-        StdOut.println(percolationItem.isOpen(3, 3));
+        return unionUF.connected(bottomIndex, topIndex);
     }
     
     // Private methods
     
+    private int 1DZeroBasedIndexFrom2DOneBasedIndexes(int i, int j) {
+        /*
+         * Suppose i and j lies within [1...N]
+         * Result index lies within [0...N)
+         */
+        if (i < 1 || i > problemSize) throw new /*java.lang.*/IndexOutOfBoundsException();
+        if (j < 1 || j > problemSize) throw new /*java.lang.*/IndexOutOfBoundsException();
+       
+        return (i - 1) * N + (j - 1);
+    }
+    
+    // main
+    
+    public static void main(String[] args) {
+        
+        Percolation percolationItem = new Percolation(3);
+        StdOut.println(percolationItem.percolates());
+    }
 }
 

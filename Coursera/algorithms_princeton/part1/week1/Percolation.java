b23d092b6b6f946f6cdc98c669233afbafee5647
Slava Zubrin <zubrin@qarea.com>
2014-10-02 16:37:24 +0300

    wip (wrong result)

--- Coursera/algorithms_princeton/part1/week1/Percolation.java
+++ Coursera/algorithms_princeton/part1/week1/Percolation.java
@@ -1,7 +1,47 @@
 public class Percolation {
     
-    private final int topIndex;
-    private final int bottomIndex;
+    // Class that represents 2D coordinate
+    private class Coord2D {
+        public int x, y;
+        public Coord2D(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    // Class that may be used to convert between 1D and 2D coordinates
+    private class CoordinatesConverter {
+    
+        public int x, y;
+        private int dimension;
+
+        // Constructors
+        public CoordinatesConverter(int N) {
+            if (N <= 0) throw new IllegalArgumentException();
+            dimension = N;
+        }
+
+        // public methods
+        public int D1FromD2(int x, int y) {
+            
+            if (x < 1 || x > dimension) throw new IndexOutOfBoundsException();
+            if (y < 1 || y > dimension) throw new IndexOutOfBoundsException();
+
+            return (x - 1) * dimension + (y - 1);
+        }
+
+        public Coord2D D2FromD1(int z) {
+            
+            if (z < 0 || z >= dimension * dimension) throw new IndexOutOfBoundsException();
+
+            int x = Math.abs(z / dimension) + 1;
+            int y = z % dimension + 1;
+            return new Coord2D(x, y);
+        }
+    }
+
+    private final int topMostFakeIndex;
+    private final int bottomMostFakeIndex;
     
     private final int problemSize;
     private final CoordinatesConverter coordConverter;
@@ -16,7 +56,7 @@ public class Percolation {
         LeftTopCornerCell,
         LeftBottomCornerCell,
         RightTopCornerCell,
-        RightBottomCorner,
+        RightBottomCornerCell,
         CommonCell
     }
 
@@ -27,10 +67,10 @@ public class Percolation {
         if (N <= 0) throw new IllegalArgumentException();
   
         problemSize = N;
-        coordConverter = CoordinatesConverter(problemSize);
+        coordConverter = new CoordinatesConverter(problemSize);
 
-        topIndex    = problemSize * problemSize + 1;
-        bottomIndex = problemSize * problemSize;
+        topMostFakeIndex    = problemSize * problemSize + 1;
+        bottomMostFakeIndex = problemSize * problemSize;
         
         int size1D = problemSize * problemSize + 2;
         
@@ -38,22 +78,22 @@ public class Percolation {
         for (int i = 0; i < size1D; i++) {
             grid1D[i] = false;
         }
-        grid1D[bottomIndex] = true;
-        grid1D[topIndex]    = true;
+        grid1D[bottomMostFakeIndex] = true;
+        grid1D[topMostFakeIndex]    = true;
         
         // prepare union
         unionUF = new WeightedQuickUnionUF(size1D);
         // connect top and bottom items to 
         for (int i = 1; i <= problemSize; i++) {
-            unionUF.union(bottomIndex, ZeroBased1DIndexFromOneBased2DIndexes(1, i)); 
-            unionUF.union(topIndex,    ZeroBased1DIndexFromOneBased2DIndexes(problemSize, i));
+            unionUF.union(bottomMostFakeIndex, coordConverter.D1FromD2(1, i)); 
+            unionUF.union(topMostFakeIndex,    coordConverter.D1FromD2(problemSize, i));
         }       
     }
     
     // Public methods
     
     public void open(int i, int j) {
-        int current1DIndex = ZeroBased1DIndexFromOneBased2DIndexes(i, j);
+        int current1DIndex = coordConverter.D1FromD2(i, j);
 
         // open the site
         grid1D[current1DIndex] = true;
@@ -61,131 +101,157 @@ public class Percolation {
         // connect it to all nearby opened sites
         CellPosition cellPosition = this.getCellPosition(i, j);
         openCell(cellPosition, current1DIndex);
+
+        if (cellPosition == CellPosition.TopSideCell
+            || cellPosition == CellPosition.RightTopCornerCell
+            || cellPosition == CellPosition.LeftTopCornerCell) 
+        {
+            // connect to the top most fake site
+            unionUF.union(current1DIndex, topMostFakeIndex);
+        }
+        else if (cellPosition == CellPosition.BottomSideCell
+                 || cellPosition == CellPosition.RightBottomCornerCell
+                 || cellPosition == CellPosition.LeftBottomCornerCell) 
+        {
+           // connect to the bottom most fake site 
+            unionUF.union(current1DIndex, bottomMostFakeIndex);
+        }
     }
     
     public boolean isOpen(int i, int j) {
-        return false; //grid2D[i][j];
+        return grid1D[coordConverter.D1FromD2(i,j)];
     }
     
     public boolean isFull(int i, int j) {
-        boolean result = false;
-        
-//        for (int k = 0; k < N; k++) {
-//            if 
-//        }
-        
-        return result;
+        int current1DCoordinate = coordConverter.D1FromD2(i,j);
+        return unionUF.connected(current1DCoordinate, topMostFakeIndex);
     }
     
     public boolean percolates() {
-        return unionUF.connected(bottomIndex, topIndex);
+        return unionUF.connected(bottomMostFakeIndex, topMostFakeIndex);
     }
     
     // Private methods
-    
-    private int ZeroBased1DIndexFromOneBased2DIndexes(int i, int j) {
-        /*
-         *
-         * Suppose i and j lies within [1...N]
-         * Result index lies within [0...N)
-         *
-         */
-        if (i < 1 || i > problemSize) throw new IndexOutOfBoundsException();
-        if (j < 1 || j > problemSize) throw new IndexOutOfBoundsException();
-       
-        return (i - 1) * problemSize + (j - 1);
-    }
-    
     private CellPosition getCellPosition(int i, int j) {
-        return CommonCell;
+        if (j == 1) {
+            if (i == 1) {
+                return CellPosition.LeftBottomCornerCell;
+            } else if (i == problemSize) {
+                return CellPosition.LeftTopCornerCell;
+            } else {
+                return CellPosition.LeftSideCell;
+            }
+        } else if (j == problemSize) {
+            if (i == 1) {
+                return CellPosition.RightBottomCornerCell;
+            } else if (i == problemSize) {
+                return CellPosition.RightTopCornerCell;
+            } else {
+                return CellPosition.RightSideCell;
+            }
+        } else {
+            if (i == 1) {
+                return CellPosition.BottomSideCell;
+            } else if (i == problemSize) {
+                return CellPosition.TopSideCell;
+            } else {
+                return CellPosition.CommonCell;
+            }
+        }
     }
 
-    private void openCell(int cellIndex1D, CellPosition cellPosition) {
+    private void openCell(CellPosition cellPosition, int cellIndex1D) {
         /*
          *
          * Connect cell with all opened neighbour cells
          * 
          */
-			
-	        // , 
-	        // RightSideCell, 
-	        // TopSideCell, 
-	        // BottomSideCell,
-	        // LeftTopCornerCell,
-	        // LeftBottomCornerCell,
-	        // RightTopCornerCell,
-	        // RightBottomCorner,
-	        // CommonCell
-
-            int topIndex1D = cellIndex1D + problemSize;
-            int botIndex1D = cellIndex1D - problemSize;
-            int lftIndex1D = cellIndex1D - 1;
-            int rhtIndex1D = cellIndex1D + 1;
-			
-			switch (cellPosition) {
-				case LeftSideCell:
-				{
-                    if (grid1D[topIndex]) {
-
-                    }
-				}
-			}
-
-        // // left
-        // if (cellPosition == LeftSideCell
-        //     || cellPosition == LeftTopCornerCell
-        //     || cellPosition == LeftBottomCornerCell) 
-        // {
-
-        // }
-
-        // // top
-
-        // // right
-
-        // // bottom
-
+        int topIndex1D      = cellIndex1D + problemSize;
+        int bottomIndex1D   = cellIndex1D - problemSize;
+        int leftIndex1D     = cellIndex1D - 1;
+        int rightIndex1D    = cellIndex1D + 1;
+   
+        switch (cellPosition) {
+            case LeftSideCell:
+                {
+                    connectCites(cellIndex1D, topIndex1D);
+                    connectCites(cellIndex1D, rightIndex1D);
+                    connectCites(cellIndex1D, bottomIndex1D);
+                }
+                break;
+            case RightSideCell:
+                {
+                    connectCites(cellIndex1D, topIndex1D);
+                    connectCites(cellIndex1D, leftIndex1D);
+                    connectCites(cellIndex1D, bottomIndex1D);
+                }
+                break;
+            case TopSideCell:
+                {
+                    connectCites(cellIndex1D, leftIndex1D);
+                    connectCites(cellIndex1D, bottomIndex1D);
+                    connectCites(cellIndex1D, rightIndex1D);
+                }
+                break;
+            case BottomSideCell:
+                {
+                    connectCites(cellIndex1D, leftIndex1D);
+                    connectCites(cellIndex1D, topIndex1D);
+                    connectCites(cellIndex1D, rightIndex1D);
+                }
+                break;
+            case LeftTopCornerCell:
+                {
+                    connectCites(cellIndex1D, rightIndex1D);
+                    connectCites(cellIndex1D, bottomIndex1D);
+                }
+                break;
+            case LeftBottomCornerCell:
+                {
+                    connectCites(cellIndex1D, rightIndex1D);
+                    connectCites(cellIndex1D, topIndex1D);
+                }
+                break;
+            case RightTopCornerCell:
+                {
+                    connectCites(cellIndex1D, leftIndex1D);
+                    connectCites(cellIndex1D, bottomIndex1D);
+                }
+                break;
+            case RightBottomCornerCell:
+                {
+                    connectCites(cellIndex1D, leftIndex1D);
+                    connectCites(cellIndex1D, topIndex1D);
+                }
+                break;
+            case CommonCell:
+            default:
+                {
+                    connectCites(cellIndex1D, leftIndex1D);
+                    connectCites(cellIndex1D, topIndex1D);
+                    connectCites(cellIndex1D, rightIndex1D);
+                    connectCites(cellIndex1D, bottomIndex1D);
+                }
+                break;
+        }
     }
 
+    private void connectCites(int first1DCoordinate, int second1DCoordinate) {
+        Coord2D first2DCoordinate  = coordConverter.D2FromD1(first1DCoordinate);
+        Coord2D second2DCoordinate = coordConverter.D2FromD1(second1DCoordinate);
+
+        if (isOpen(first2DCoordinate.x, first2DCoordinate.y) 
+            && isOpen(second2DCoordinate.x, second2DCoordinate.y))
+        {
+            unionUF.union(first1DCoordinate, second1DCoordinate);
+        }
+    }
+
+
     // main
-    
     public static void main(String[] args) {
         
         Percolation percolationItem = new Percolation(3);
         StdOut.println(percolationItem.percolates());
-    }
-}
-
-class CoordinatesConverter {
-    
-    public int x, y;
-
-    private int dimension;
-
-    // Constructors
-
-    public CoordinatesConverter(int N) {
-
-        if (N <= 0) throw new IllegalArgumentException();
-
-        dimension = N;
-    }
-
-    // public methods
-
-    public int D1FromD2(int x, int y) {
-        return 0;
-    }
-
-    public Coord2D D2FromD1(int x) {
-        return Coord2D(1,1);
-    }
-}
-
-class Coord2D {
-    public int x, y;
-    public Coord2D (int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
---
+++ Coursera/algorithms_princeton/part1/week1/PercolationStats.java
@@ -0,0 +1,81 @@
+public class PercolationStats {
+
+    private double[] thresholds;
+    private int iterationsNumber;
+
+    // Public
+    public PercolationStats(int N, int T)    // perform T independent computational experiments on an N-by-N grid
+    {
+        if (N <= 0) throw new IllegalArgumentException();
+        if (T <= 0) throw new IllegalArgumentException();
+
+        iterationsNumber = T;
+        thresholds = new double[iterationsNumber];
+
+        for (int i = 0; i < iterationsNumber; i++) {
+            
+            StdOut.printf("%3d: ", i);
+            
+            double threshold = experiment(N);
+            thresholds[i] = threshold;
+        }
+    }
+
+    public double mean()                     // sample mean of percolation threshold
+    {
+        return StdStats.mean(thresholds);
+    }
+    public double stddev()                   // sample standard deviation of percolation threshold
+    {
+        return StdStats.stddev(thresholds);
+    }
+    public double confidenceLo()             // returns lower bound of the 95% confidence interval
+    {
+        return mean() - 1.96 * stddev() / Math.sqrt(iterationsNumber);
+    }
+    public double confidenceHi()             // returns upper bound of the 95% confidence interval
+    {
+        return mean() + 1.96 * stddev() / Math.sqrt(iterationsNumber);
+    }
+    public static void main(String[] args)   // test client
+    {
+//        int N = Integer.parseInt(args[0]);
+//        int T = Integer.parseInt(args[1]);
+        int N = 200;
+        int T = 100;
+        
+        PercolationStats ps = new PercolationStats(N, T);
+
+        StdOut.printf("mean                    = %.16f\n", ps.mean());
+        StdOut.printf("stddev                  = %.16f\n", ps.stddev());
+        StdOut.printf("95% confidence interval = %.16f, %.16f", ps.confidenceLo(), ps.confidenceHi());
+    }
+
+    // Private
+    private double experiment(int N)
+    {
+        int counter = 0;
+        Percolation p = new Percolation(N);
+        boolean isPercolated = p.percolates();
+        
+        StdOut.println("Initially percolates");
+        
+        while (isPercolated == false)
+        {
+            int x = StdRandom.uniform(N) + 1;
+            int y = StdRandom.uniform(N) + 1;
+
+            if (!p.isOpen(x, y))
+            {
+                p.open(x, y);
+                counter += 1;
+            }
+            
+            isPercolated = p.percolates();
+        }
+        
+//        StdOut.println(counter);
+ 
+        return counter / (N * N);
+    }
+}
